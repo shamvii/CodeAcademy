96c190f3bfb550289b64994e14cf75f86a087776
shamvii <shambhavishukla24@gmail.com>
2016-11-29 18:51:06 -0500

    Inheritance in JS

---
+++ CodeAcademy/inheritance.js
@@ -0,0 +1,25 @@
+//Inheritance in JS
+
+function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+}
+Animal.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+
+// create a Penguin constructor here
+function Penguin(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+};
+
+// create a sayName method for Penguins here
+Penguin.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+// our test code
+var theCaptain = new Penguin("Captain Cook", 2);
+theCaptain.sayName();
