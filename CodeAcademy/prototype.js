f0a8e39c266d8c4a828c6cb9b730ec0b0a1d230c
shamvii <shambhavishukla24@gmail.com>
2016-11-29 18:49:10 -0500

    UsingPrototypeinJS

---
+++ CodeAcademy/prototype.js
@@ -0,0 +1,13 @@
+function Dog (breed) {
+  this.breed = breed;
+};
+var buddy = new Dog("golden Retriever");
+Dog.prototype.bark = function() {
+  console.log("Woof");
+};
+buddy.bark();
+
+// here we make snoopy
+var snoopy = new Dog("Beagle");
+
+snoopy.bark();
